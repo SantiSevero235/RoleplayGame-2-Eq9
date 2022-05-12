9503949b352491276d333a08414da224075643f7
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2022-05-11 22:52:40 -0300

    cambio de lugar de tests

--- src/test/Test.Library/EquipmentTest.cs
+++ src/test/Test.Library/EquipmentTest.cs
@@ -25,44 +25,6 @@ namespace Test.Library
             axeTest = new Axe();
         }
 
-        // Se prueba si al equipar un baculo se modifica correctamente la defensa
-        [Test]
-        public void WizardEquipStaffDefense()
-        {
-            int expected = wizardTest.DefenseValue + staffTest.DefenseValue;
-            wizardTest.Equip(staffTest);
-            Assert.AreEqual(expected, wizardTest.DefenseValue);
-        }
-
-        // Se prueba si al equipar un baculo se modifica correctamente el ataque
-        [Test]
-        public void WizardEquipStaffAttack()
-        {
-            int expected = wizardTest.AttackValue + staffTest.AttackValue;
-            wizardTest.Equip(staffTest);
-            Assert.AreEqual(expected, wizardTest.AttackValue);  
-        }
-
-        // Se prueba si al desequipar un baculo se modifica correctamente el ataque
-        [Test]
-        public void WizardUnequipStaffAttack()
-        {
-            int expected = wizardTest.AttackValue;
-            wizardTest.Equip(staffTest);
-            wizardTest.Unequip(staffTest);
-            Assert.AreEqual(expected, wizardTest.AttackValue);
-        }
-
-        // Se prueba si al desequipar un baculo se modifica correctamente la defensa
-        [Test]
-        public void WizardUnequipStaffDefense()
-        {
-            int expected = wizardTest.DefenseValue;
-            wizardTest.Equip(staffTest);
-            wizardTest.Unequip(staffTest);
-            Assert.AreEqual(expected, wizardTest.DefenseValue);
-        }
-
         // Se prueba si al equipar un item de ataque se modifica correctamente la defensa
         [Test]
         public void DwarfEquipAxeDefense()
